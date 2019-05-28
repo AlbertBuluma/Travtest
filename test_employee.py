d057286b573a2803d9f75eda40b0f92c6edf5bce
AlbertBuluma <albert.buluma1@gmail.com>
2019-05-28 10:56:28 +0300

    Modified employee test file.

--- test_employee.py
+++ test_employee.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch
 from employee import Employee
 
 
@@ -15,7 +14,7 @@ class TestEmployee(unittest.TestCase):
 
     def setUp(self):
         print('setUp')
-        self.emp_1 = Employee('Corey', 'Schafer', 50000)
+        self.emp_1 = Employee('Albert', 'Buluma', 50000)
         self.emp_2 = Employee('Sue', 'Smith', 60000)
 
     def tearDown(self):
@@ -23,24 +22,24 @@ class TestEmployee(unittest.TestCase):
 
     def test_email(self):
         print('test_email')
-        self.assertEqual(self.emp_1.email, 'Corey.Schafer@email.com')
+        self.assertEqual(self.emp_1.email, 'Albert.Buluma@email.com')
         self.assertEqual(self.emp_2.email, 'Sue.Smith@email.com')
 
         self.emp_1.first = 'John'
         self.emp_2.first = 'Jane'
 
-        self.assertEqual(self.emp_1.email, 'John.Schafer@email.com')
+        self.assertEqual(self.emp_1.email, 'John.Buluma@email.com')
         self.assertEqual(self.emp_2.email, 'Jane.Smith@email.com')
 
     def test_fullname(self):
         print('test_fullname')
-        self.assertEqual(self.emp_1.fullname, 'Corey Schafer')
+        self.assertEqual(self.emp_1.fullname, 'Corey Buluma')
         self.assertEqual(self.emp_2.fullname, 'Sue Smith')
 
         self.emp_1.first = 'John'
         self.emp_2.first = 'Jane'
 
-        self.assertEqual(self.emp_1.fullname, 'John Schafer')
+        self.assertEqual(self.emp_1.fullname, 'John Buluma')
         self.assertEqual(self.emp_2.fullname, 'Jane Smith')
 
     def test_apply_raise(self):
@@ -51,21 +50,7 @@ class TestEmployee(unittest.TestCase):
         self.assertEqual(self.emp_1.pay, 52500)
         self.assertEqual(self.emp_2.pay, 63000)
 
-    def test_monthly_schedule(self):
-        with patch('employee.requests.get') as mocked_get:
-            mocked_get.return_value.ok = True
-            mocked_get.return_value.text = 'Success'
-
-            schedule = self.emp_1.monthly_schedule('May')
-            mocked_get.assert_called_with('http://company.com/Schafer/May')
-            self.assertEqual(schedule, 'Success')
-
-            mocked_get.return_value.ok = False
-
-            schedule = self.emp_2.monthly_schedule('June')
-            mocked_get.assert_called_with('http://company.com/Smith/June')
-            self.assertEqual(schedule, 'Bad Response!')
-
+    
 
 if __name__ == '__main__':
     unittest.main()
